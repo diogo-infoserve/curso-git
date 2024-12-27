6daf83f71e6fe47023604307317446d12b405884
diogo <diogolimadavalos@gmail.com>
2024-12-27 08:16:41 -0300

    Modificacion de codigo

--- teste.py
+++ teste.py
@@ -1,1 +1,6 @@
-print(2+2)
+def datos(nombre, apellido):
+    print("Tu nombre es " + nombre + " "+ apellido)
+
+nombre=input("Ingrese nombre: ")
+apellido=input("\nIngrese apellido: ")
+datos(nombre, apellido)
